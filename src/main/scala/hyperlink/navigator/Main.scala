685403a327c9ebd32aa42d3f286460d26a1c4f54
Kenneth Monteiro <kenneth.d.monteiro@gmail.com>
2026-07-23 20:26:33 +0100

    added url validation to the stream

--- src/main/scala/hyperlink/navigator/Main.scala
+++ src/main/scala/hyperlink/navigator/Main.scala
@@ -4,7 +4,13 @@ package ice.finance
 
 import cats.effect.IOApp
 import hyperlink.navigator.repository.FileReaderRepository
+import hyperlink.navigator.service.InputValidatorService
 
 object Main extends IOApp.Simple {
-  def run = StreamingApp.stream("urls.csv", "extracted-urls.csv", FileReaderRepository())
+  def run = StreamingApp.stream(
+    "urls.csv",
+    "extracted-urls.csv",
+    FileReaderRepository(),
+    InputValidatorService()
+  )
 }
--- src/main/scala/hyperlink/navigator/StreamingApp.scala
+++ src/main/scala/hyperlink/navigator/StreamingApp.scala
@@ -3,21 +3,30 @@ package hyperlink.navigator
 import cats.effect.IO
 import fs2.io.file.{Files, Path}
 import fs2.{Stream, text}
+import hyperlink.navigator.domain.ValidatedUrl
 import hyperlink.navigator.repository.FileReaderRepository
+import hyperlink.navigator.service.InputValidatorService
 
 object StreamingApp {
   def stream(
-              inputFilePath: String,
-              resultsFilePath: String,
-              fileReaderRepository: FileReaderRepository,
-            ): IO[Unit] = {
-    val fileReadingStream: Stream[IO, String] =
-      fileReaderRepository
+    inputFilePath: String,
+    resultsFilePath: String,
+    fileReaderRepository: FileReaderRepository,
+    inputValidatorService: InputValidatorService
+  ): IO[Unit] = {
+    val fileReadingStream: Stream[IO, ValidatedUrl] = fileReaderRepository
       .getLines(inputFilePath)
-      .map(rawData => rawData.value)
-      .intersperse("\n")
+      .map(rawData => inputValidatorService.validateRow(rawData.value))
+      .flatMap {
+        case Left(error) =>
+          Stream.exec(IO.println(s"File error=$error"))
+        case Right(value) =>
+          Stream.emit(value)
+      }
 
     fileReadingStream
+      .map(_.uri.toString)
+      .intersperse("\n")
       .through(text.utf8.encode)
       .through(Files[IO].writeAll(Path(resultsFilePath)))
       .compile
